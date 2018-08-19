909a40a957880ba70a8b543137e42e277424b39f
doodthedoodthedood <37311302+doodthedoodthedood@users.noreply.github.com>
2018-08-19 17:31:42 -0400

    Update fun.py

--- Fun/fun.py
+++ Fun/fun.py
@@ -1,16 +1,18 @@
-import discord
 from discord.ext import commands
-
+ 
+ 
 class fun:
-    """A cog that does fun stuff"""
-
+    """Fun commands"""
+ 
     def __init__(self, bot):
         self.bot = bot
-
-   @commands.command(no_pm=True)
+ 
+    @commands.command(no_pm=True)
     async def repeat(self, *, text):
         """Copies your words"""
         await self.bot.say(text)
-
+ 
+ 
 def setup(bot):
-    bot.add_cog(fun(bot))
+    n = fun(bot)
+    bot.add_cog(n)
