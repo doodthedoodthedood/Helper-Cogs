25e2e092dd27ec77162341251a4305efabad32ff
doodthedoodthedood <37311302+doodthedoodthedood@users.noreply.github.com>
2018-08-22 17:09:26 -0400

    Update hug.py

--- cogs/hug/hug.py
+++ cogs/hug/hug.py
@@ -8,7 +8,7 @@ class Hug:
             
             
         @commands.command()
-        async def _hug(self, *, random = None):
+        async def _hug(self, *, random = False):
             if random:
                 self.bot.say('***FAMLIYA HUG INCOMING!!***:hugging: :heart:')
             else:
