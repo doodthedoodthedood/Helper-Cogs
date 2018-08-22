a11ae281de45656729e10892599a6970949cdab4
doodthedoodthedood <37311302+doodthedoodthedood@users.noreply.github.com>
2018-08-22 16:46:41 -0400

    Create hug.py

---
+++ cogs/hug/hug.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+
+class Hug:
+        """Family Hug!"""
+        def __init__(self, bot):
+            self.bot = bot
+            
+            
+            
+        @commands.command()
+        async def hug(self, *, random):
+            self.bot.say('***FAMLIYA HUG INCOMING!!***:hugging: :heart:')
+            
+            
+def setup(bot):
+    n = Repeat(bot)
+    bot.add_cog(n)
