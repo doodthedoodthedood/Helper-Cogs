e33e793d8e4301f815cfc955e9eebf7c9d759f4a
doodthedoodthedood <37311302+doodthedoodthedood@users.noreply.github.com>
2018-09-02 08:42:02 -0400

    Add files via upload

---
+++ emoji/emoji.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+
+class Emoji:
+        """States any emoji"""
+        def __init__(self, bot):
+            self.bot = bot
+        
+        
+        
+        @commands.group(pass_context=True, no_pm=True)
+        async def emoji(self, ctx):
+         if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+            
+        @emoji.command(no_pm=True)
+        async def convert(self, emoji):
+            """Converts text into an emoji"""
+            await self.bot.say(":{}:").format(emoji)
+            
+        @emoji.command(no_pm=True)
+        async def list(self, ctx):
+            self.bot.say(emojis)
+            
+            
+def setup(bot):
+        n = emoji(bot)
+        bot.add_cog(n)
